a3cf625702ef85f5dcd2250928545ce8fabbb36c
HappyAndSad <32348628+HappyAndSad@users.noreply.github.com>
2023-06-20 09:46:55 +0800

    数据访问层搭建

---
+++ YSFB.Data/YSFB.Data/Database/MysqlDatabase.cs
@@ -0,0 +1,32 @@
+﻿// ********************************************************
+// MysqlDatabase.cs
+// Date: 2023/6/16 22:43
+// Author: HappyAndSad 
+// Copyright (c) 2023 MIT 
+// ********************************************************
+using System;
+namespace YSFB.Data.FS.Database
+{
+    /// <summary>
+    /// Mysql数据库链接实现
+    /// </summary>
+	public class MysqlDatabase:IDataBase
+	{
+
+        #region 属性
+        public IFreeSql fsql;
+        #endregion
+
+
+        public MysqlDatabase(string connectionString)
+		{
+            fsql = new FreeSql.FreeSqlBuilder()
+                .UseConnectionString(FreeSql.DataType.Sqlite, connectionString)
+                .UseAutoSyncStructure(true) //自动迁移实体的结构到数据库
+                .Build();
+        }
+
+		
+	}
+}
+
